07e5f84294078af62024aeeb71cb99efb233fd23
HemlockPham7 <london1952hoang@gmail.com>
2024-11-26 19:51:32 +0700

    "Feat: Check authentication"

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,12 +1,16 @@
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
-import { loginService, registerService } from "@/services";
-import { createContext, useState } from "react";
+import { checkAuthService, loginService, registerService } from "@/services";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
   const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+  const [auth, setAuth] = useState({
+    authenticate: false,
+    user: null,
+  });
 
 
   async function handleRegisterUser(event) {
@@ -19,7 +23,54 @@ export default function AuthProvider({ children }) {
     event.preventDefault();
     const data = await loginService(signInFormData);
     console.log(data, "datadatadatadatadata");
+
+    if (data.success) {
+      sessionStorage.setItem(
+        "accessToken",
+        JSON.stringify(data.data.accessToken)
+      );
+      setAuth({
+        authenticate: true,
+        user: data.data.user,
+      });
+    } else {
+      setAuth({
+        authenticate: false,
+        user: null,
+      });
+    }
   }
+
+  async function checkAuthUser() {
+    try {
+      const data = await checkAuthService();
+      if (data.success) {
+        setAuth({
+          authenticate: true,
+          user: data.data.user,
+        });
+      } else {
+        setAuth({
+          authenticate: false,
+          user: null,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      if (!error?.response?.data?.success) {
+        setAuth({
+          authenticate: false,
+          user: null,
+        });
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkAuthUser();
+  }, []);
+
+  console.log(auth, "gf");
 
   return (
     <AuthContext.Provider
