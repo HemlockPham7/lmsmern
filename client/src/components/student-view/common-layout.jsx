4b0a2d8fd24e2b88d4ff2bd154f0baf2be49ea4b
HemlockPham7 <london1952hoang@gmail.com>
2024-11-28 11:19:49 +0700

    Feat: Create route-guard for authen

---
+++ client/src/components/student-view/common-layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+function StudentViewCommonLayout() {
+  return (
+    <div>
+      StudentViewCommonLayout
+
+      <Outlet />
+    </div>
+  );
+}
+
+export default StudentViewCommonLayout;
---
+++ client/src/pages/instructor/index.jsx
@@ -0,0 +1,8 @@
+export default function InstructorDashboardpage() {
+
+  return (
+    <div>
+      InstructorDashboardpage
+    </div>
+  );
+}
